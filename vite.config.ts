f97b7400f4c4788a861a58ba635932cd79e15bb6
srivaishnavienterprise2022-tech <srivaishnavienterprise2022@gmail.com>
2026-01-21 02:12:08 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,11 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    sourcemap: false
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
   }
 });
