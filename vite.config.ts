420c46087b0aae881774fe65958d5de212242af2
srivaishnavienterprise2022-tech <srivaishnavienterprise2022@gmail.com>
2026-01-21 01:28:33 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env': process.env
+    // ఇది బ్రౌజర్‌లోని కోడ్‌లో 'process.env.API_KEY' ని రియల్ వాల్యూతో రీప్లేస్ చేస్తుంది
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
   },
   build: {
     outDir: 'dist',
